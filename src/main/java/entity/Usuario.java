b03eaedac359f98b3401a93594717ece8be95819
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-06-27 16:01:24 -0500

    Feat: Se añadio la clase usuario

--- src/main/java/entity/Usuario.java
+++ src/main/java/entity/Usuario.java
@@ -17,6 +17,35 @@ public class Usuario {
         this.idrol = idrol;
     }
 
-    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getIdrol() {
+        return idrol;
+    }
+    public void setIdrol(int idrol) {
+        this.idrol = idrol;
+    }
 
 }
