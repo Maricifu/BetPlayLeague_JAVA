a4e3fff14e0623657d8fab8183abdd468165350c
Maricifu <152333294+Maricifu@users.noreply.github.com>
2024-06-27 17:29:16 -0500

    :feat: main start login structure

--- src/main/java/com/betplay/Main.java
+++ src/main/java/com/betplay/Main.java
@@ -1,7 +1,54 @@
 package com.betplay;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Controllers
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Menú inicial
+        while (true) {
+            limpiarPantalla();
+            System.out.println("  ██████╗ ███████╗████████╗██████╗ ██╗      █████╗ ██╗   ██╗    ██╗     ███████╗ █████╗  ██████╗ ██╗   ██╗███████╗\n" + //
+                                " ██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██║     ██╔══██╗╚██╗ ██╔╝    ██║     ██╔════╝██╔══██╗██╔════╝ ██║   ██║██╔════╝\n" + //
+                                " ██████╔╝█████╗     ██║   ██████╔╝██║     ███████║ ╚████╔╝     ██║     █████╗  ███████║██║  ███╗██║   ██║█████╗  \n" + //
+                                " ██╔══██╗██╔══╝     ██║   ██╔═══╝ ██║     ██╔══██║  ╚██╔╝      ██║     ██╔══╝  ██╔══██║██║   ██║██║   ██║██╔══╝  \n" + //
+                                " ██████╔╝███████╗   ██║   ██║     ███████╗██║  ██║   ██║       ███████╗███████╗██║  ██║╚██████╔╝╚██████╔╝███████╗\n" + //
+                                " ╚═════╝ ╚══════╝   ╚═╝   ╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝       ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚══════╝");
+            System.out.println("                                                                                                                ");
+            System.out.println(" ,-------------------------,");
+            System.out.println(" |  Seleccione una opcion: |");
+            System.out.println(" |-------------------------|");
+            System.out.println(" | 1. Login                |");
+            System.out.println(" | 2. Salir                |");
+            System.out.println(" '-------------------------'");
+            System.out.print("Opción: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    // l.start();
+                    break;
+                case 2:
+                    System.out.println("Saliendo de la aplicación...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Opción inválida. Intente de nuevo.");
+            }
+        }
     }
+
+    // limpiar pantalla
+    private static void limpiarPantalla() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+    }
+
 }
