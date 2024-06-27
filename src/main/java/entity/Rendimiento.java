d63c309fcd56fc61447f46b7ae4d49f59885a8b7
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-06-27 15:50:34 -0500

    Style: Se borro texto de referencia

--- src/main/java/entity/Rendimiento.java
+++ src/main/java/entity/Rendimiento.java
@@ -69,17 +69,4 @@ public class Rendimiento {
         this.asistencias = asistencias;
     }
 
-    
-
-    
-
-    // id : Identificador único del rendimiento.
-    // jugador : Jugador al que corresponde el rendimiento.
-    // partido : Partido en el que se evaluó el rendimiento.
-    // minutosJugados : Minutos jugados por el jugador en el partido.
-
-    // goles : Número de goles anotados por el jugador en el partido.
-    // asistencias : Número de asistencias del jugador en el partido.
-    // tarjetasAmarillas : Número de tarjetas amarillas recibidas por el jugador en el partido.
-    // tarjetasRojas : Número de tarjetas rojas recibidas por el jugador en el partido.
     }
