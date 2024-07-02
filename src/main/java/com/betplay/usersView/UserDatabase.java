d2ea03e0c1934b5a696b05b095b7d0b4eba90581
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-07-02 15:06:26 -0500

    modificados:     src/main/java/com/betplay/usersView/UserDatabase.java 	modificados:     target/classes/com/betplay/usersView/UserDatabase.class

--- src/main/java/com/betplay/usersView/UserDatabase.java
+++ src/main/java/com/betplay/usersView/UserDatabase.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.betplay.model.entity.Arbitro;
 import com.betplay.model.entity.Rol;
 import com.betplay.model.entity.Usuario;
 
@@ -24,17 +23,23 @@ public class UserDatabase {
         Rol entrenadorRol = new Rol("2", "Entrenador", null);
         Rol aficionadoRol = new Rol("3", "Aficionado", null);
         Rol ArbitroRol = new Rol("4", "Árbitro", null);
+        Rol MedicoRol = new Rol("5", "Médico", null);
+        Rol PeriodistaRol = new Rol("6", "Periodista", null);
 
 
         Usuario admin = new Usuario("1", "Admin", "admin123", "admin123", adminRol);
         Usuario entrenador = new Usuario("2", "Entrenador", "coach123", "coach123", entrenadorRol);
         Usuario aficionado = new Usuario("3", "Aficionado", "fan123", "fan123", aficionadoRol);
         Usuario arbitro = new Usuario("4", "Arbitro", "arbitro123", "arbitro123", ArbitroRol);
+        Usuario medico = new Usuario("5", "Médico", "medico123", "medico123", MedicoRol);
+        Usuario periodista = new Usuario("6", "Periodista", "periodista123", "periodista123", PeriodistaRol);
 
         usuarios.put(admin.getId(), admin);
         usuarios.put(entrenador.getId(), entrenador);
         usuarios.put(aficionado.getId(), aficionado);
         usuarios.put(arbitro.getId(),arbitro);
+        usuarios.put(medico.getId(),medico);
+        usuarios.put(periodista.getId(),periodista);
     }
 
     public void addUsuario(Usuario usuario) {
